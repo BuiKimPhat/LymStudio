953115ca2b42a71930e231c108051d4a1d0839d3
root <root@LeoTheThird.localdomain>
2022-08-27 11:30:08 +0700

    bootstrap

---
+++ components/footer.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+      <footer>
+        <div className="container text-center">
+          <div className="w-100 text-center">
+            <a href="">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </div>
+          <h4>© 2022 Copyright: LymStudio</h4>
+        </div>
+      </footer>
+    )
+  }
