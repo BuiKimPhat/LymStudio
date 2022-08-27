bf0ea0970fcabac1399c75a00634158ff3e0abcb
BuiKimPhat <buikimphat42@gmail.com>
2022-08-27 20:06:23 +0700

    ui

--- components/footer.js
+++ components/footer.js
@@ -4,10 +4,10 @@ export default function Footer() {
         <div className="container text-center">
           <div className="w-100 text-center">
             <a href="">
-              <i class="fab fa-facebook-f"></i>
+              <i className="fab fa-facebook-f"></i>
             </a>
             <a href="">
-              <i class="fab fa-instagram"></i>
+              <i className="fab fa-instagram"></i>
             </a>
           </div>
           <h4>© 2022 Copyright: LymStudio</h4>
